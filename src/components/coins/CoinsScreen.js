7e9ba2d0240083f96cf16800becfbcb8b7290adf
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-04-11 16:30:47 -0500

    change screens

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';;
+import {View, Text, Pressable, StyleSheet} from 'react-native';;
 
 class CoinsScreen extends Component{
+    handlePress = () =>{
+        console.log("go to detail",this.props);
+        this.props.navigation.navigate('CoinDetail');
+    }
     render(){
         return(
-            <View>
-                <Text>Coins Screen</Text>
+            <View style={styles.container}>
+                <Text style={styles.titleText}>Coins Screen</Text>
+                <Pressable style={styles.btn} onPress={this.handlePress}>
+                    <Text style={styles.btnText}>Ir a details</Text>
+                </Pressable>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'red',
+    },
+    titleText:{
+        color:'#fff',
+        textAlign:'center'
+    },
+    btn:{
+        padding:8,
+        backgroundColor:'blue',
+        borderRadius:8,
+        margin:16
+    },
+    btnText:{
+        color:'#fff',
+        textAlign:'center'
+
+    }
+});
+
 export default CoinsScreen;
