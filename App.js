c81f05f32ef27c4a5a0145993a3b49ecfb420670
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-04-16 15:18:45 -0500

    added tab navigation

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import CoinsStack from 'cryptoTracker/src/components/coins/CoinsStack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';  
+import Colors from 'cryptoTracker/src/res/colors';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-        <CoinsStack/>
+      <Tabs.Navigator
+        tabBarOptions={{
+            tintColor:'#fefefe',
+            style:{
+                backgroundColor:Colors.blackPearl
+            }
+        }}    
+      >
+        <Tabs.Screen
+            name="Coins"
+            component={CoinsStack}
+            options={{
+                tabBarIcon:({size,color})=>(
+                    <Icon 
+                        name="bank" 
+                        size={size}
+                        color={color}
+                    />
+                )
+            }}
+          />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
